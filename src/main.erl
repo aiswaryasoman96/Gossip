208200df255c315371febd23b0253a4054c9de32
Christy George <christygeorgeind@gmail.com>
2022-10-08 20:27:27 -0400

    Refactor main

--- src/main.erl
+++ src/main.erl
@@ -6,35 +6,18 @@
 getConnectedActors(NeighbourMap) ->
     receive
         MyPID ->
-            % io:fwrite("~n Map ~w", [maps:to_list(NeighbourMap)]),
-            % io:fwrite("~n Caller PID ~w", [MyPID]),
-            % io:fwrite("~n Neighbours ~w", [maps:get(MyPID, NeighbourMap)]),
             MyPID ! maps:get(MyPID, NeighbourMap)
     end,
     getConnectedActors(NeighbourMap).
             
 
 start(NumNodes, Topology, Algorithm)->
-    case Algorithm of 
-        gossip -> 
-            io:fwrite("Starting the Gossip...."),
-            case Topology of 
-                line ->
-                    line:build(NumNodes,"gossip");
-                full ->
-                    full:build(NumNodes,"gossip");
-                '2D' ->
-                    twoDGrid:build(NumNodes,"gossip")
-            end;        
+    case Topology of 
+        line ->
+            line:build(Algorithm, NumNodes);
+        full ->
+            full:build(Algorithm, NumNodes);
+        '2D' ->
+            twoDGrid:build(Algorithm, NumNodes)
+    end.       
 
-        push_sum ->
-            case Topology of 
-                line ->
-                    line:build(NumNodes,"push-Sum");
-                full ->
-                    full:build(NumNodes,"push-Sum");
-                '2D' ->
-                    twoDGrid:build(NumNodes,"push-Sum")
-            end
-    end.
-    
