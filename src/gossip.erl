de6a8b90aec2a160250c336348e15210edd7a03b
Christy George <christygeorgeind@gmail.com>
2022-10-10 17:11:52 -0400

    Fix bugs in the gossip parallelism

--- src/gossip.erl
+++ src/gossip.erl
@@ -1,32 +1,50 @@
 -module(gossip).
--export([start/1, spreadRumor/2, listenToRumor/3]).
+-export([start/1, spreadRumor/2, listenToRumor/4]).
 
 
 spreadRumor(ConnectedNodes, Rumor) ->
-    Len = length(ConnectedNodes),
-    Nth = rand:uniform(Len),
-    ChosenNeighbour = lists:nth(Nth, ConnectedNodes),
-    ChosenNeighbour ! Rumor,
-    spreadRumor(ConnectedNodes, Rumor).
+    receive
+        {PID, true} -> 
+            Len = length(ConnectedNodes),
+            Nth = rand:uniform(Len),
+            ChosenNeighbour = lists:nth(Nth, ConnectedNodes),
+            IsProcessAlive = is_process_alive(ChosenNeighbour),
 
-listenToRumor(_, _, RumorCount) when RumorCount > 20->
+            if IsProcessAlive == true ->
+                ChosenNeighbour ! Rumor
+            ;true ->
+                self() ! {PID, true},
+                spreadRumor(ConnectedNodes, Rumor)
+            end,
+
+            spreadRumor(ConnectedNodes, Rumor);
+        {PID, false} -> 
+            io:fwrite("~n~w stopped spreading", [PID])
+    end.
+
+listenToRumor(_, _, SpreadPID, RumorCount) when RumorCount > 10->
     % Stop listening and spreading!!!
-    io:fwrite("~nHad enough of the Rumors!!! Quitting ~w", [self()]);
+    SpreadPID ! {self(), false},
+    io:fwrite("~n~w got 10 Rumors!!!", [self()]);
 
 
-listenToRumor(Rumor, ConnectedNodes, RumorCount) when RumorCount =< 20->
+listenToRumor(Rumor, ConnectedNodes, SpreadPID,RumorCount) when RumorCount =< 10->
     receive
         Rumor ->
-            listenToRumor(Rumor, ConnectedNodes, RumorCount + 1)
+            io:fwrite("~n~w Got ~w Rumor", [self(), RumorCount]),
+            SpreadPID ! {self(), true},
+            listenToRumor(Rumor, ConnectedNodes, SpreadPID, RumorCount + 1)
     end.
 
 start(_) ->
     receive
         Rumor ->
+            io:fwrite("~n~w Got First Rumor", [self()]),
             getNeighbours ! self(),
             receive
                 ConnectedNodes -> 
-                    spawn(gossip, spreadRumor, [ConnectedNodes, Rumor]),
-                    listenToRumor(Rumor, ConnectedNodes, 2)
+                    SpreadPID = spawn(gossip, spreadRumor, [ConnectedNodes, Rumor]),
+                    SpreadPID ! {self(), true},
+                    listenToRumor(Rumor, ConnectedNodes, SpreadPID,  2)
             end
     end.
